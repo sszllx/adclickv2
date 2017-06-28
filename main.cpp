0afd6a39f5102d521cce6dd7a2f19228104a25c1
sszllx <sszllx@gmail.com>
2017-06-29 06:30:33 +0800

    add bloom filter
    add bloom filter

--- main.cpp
+++ main.cpp
@@ -1,21 +1,13 @@
 #include <QCoreApplication>
 #include <QObject>
+#include <QString>
+#include <QDebug>
 
 #include "click.h"
-
-#if 0
-void handle(int sig)
-{
-    Q_UNUSED(sig)
-    qApp->exit();
-}
-#endif
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    signal(SIGINT, NULL);
 
     Click click;
     click.startRequest();
