4b4a1569dce0cee67688624a95864f2fe4e8a0a6
sszllx <sszllx@gmail.com>
2017-07-21 23:31:18 +0800

    set chizicheng
    set chizicheng

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     Click *click = Click::getInstance();
+    click->setWangmeng(argv[1]);
     click->startRequest();
 
     return a.exec();
